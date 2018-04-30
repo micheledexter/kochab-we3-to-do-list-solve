9fd073f1ffc44b66c4a29b05459b5c135512eb7d
Shely Dexter <micheleannedexter@gmail.com>
2018-04-30 08:57:31 -0500

    add bootstrap and angular scripts and get server set up

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// serve static files
+app.use(express.static('server/public'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
