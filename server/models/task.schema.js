511d1dd1df641602ebe3f3d08f349f33c8334ec8
Shely Dexter <micheleannedexter@gmail.com>
2018-04-30 10:14:48 -0500

    task post funcitonality

--- server/models/task.schema.js
+++ server/models/task.schema.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    task_name: {type: String},
+    task_name: {type: String, required: true},
     priority: {type: String},
     due_date: {type: Date}
 })
 
-module.exports = mondoose.model('task', TaskSchema);
+module.exports = mongoose.model('task', TaskSchema);
