2e43b7705669bcb55ce57b2395a72407e9f9cf1c
Shely Dexter <micheleannedexter@gmail.com>
2018-04-30 10:16:16 -0500

    task post funcitonality

--- server/models/task.schema.js
+++ server/models/task.schema.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const TaskSchema = new Schema({
     task_name: {type: String, required: true},
     priority: {type: String},
+    complete: {type: Boolean, default: false},
     due_date: {type: Date}
 })
 
